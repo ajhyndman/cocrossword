517fa9be6b7e1871380dbe78761e7e9a7f8f98f7
Andrew Hyndman <ajhyndman@hotmail.com>
2023-09-03 21:42:57 -0400

    Flatten component files

--- src/components/DropZone.tsx
+++ src/components/DropZone.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { input, label } from './index.module.css';
+import styles from './DropZone.module.css';
 
 type Props = {
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
 export default ({ onChange }: Props) => (
-  <label className={label}>
+  <label className={styles.label}>
     <span className="material-icons" style={{ fontSize: 56 }}>
       upload
     </span>
     <input
       type="file"
       accept=".puz"
-      className={input}
+      className={styles.input}
       onChange={onChange}
     ></input>
   </label>
---
+++ src/views/UploadView.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import DropZone from '../components/DropZone';
+import styles from './UploadView.module.css';
+
+export default () => (
+  <div className={styles.container}>
+    <DropZone onChange={(event) => console.log(event.target.files)} />
+    <p className={styles.caption}>Upload a .puz file to start a new game</p>
+  </div>
+);
