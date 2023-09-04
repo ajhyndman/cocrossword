e105e0865aed2e3842e95797ebe3d44adde9cb97
Andrew Hyndman <ajhyndman@hotmail.com>
2023-09-03 21:43:23 -0400

    Draft Clue component

---
+++ src/components/Clue.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Clue.module.css';
+
+type Props = {
+  content: string;
+  index: number;
+  isActive?: boolean;
+};
+
+export default ({ index, content, isActive = false }: Props) => (
+  <li className={`${styles.listItem} ${isActive ? styles.active : ''}`}>
+    <span className={styles.index}>{index}</span>
+    <span>{content}</span>
+  </li>
+);
