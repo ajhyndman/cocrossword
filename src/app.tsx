10a94b80ac5c7473faf09e22e2118a009a99c9b1
Andrew Hyndman <ajhyndman@hotmail.com>
2023-09-27 19:26:49 -0400

    Usability tweaks for iOS devices

--- src/app.tsx
+++ src/app.tsx
@@ -5,6 +5,17 @@ import { Provider } from 'react-redux';
 import Solver from './views/Solver';
 import { createStore } from './store/store';
 
+// disable pinch zoom on iOS devices
+document.addEventListener(
+  'touchmove',
+  (event) => {
+    if ('scale' in event && event.scale !== 1) {
+      event.preventDefault();
+    }
+  },
+  { passive: false },
+);
+
 const store = createStore();
 
 const App = () => (
