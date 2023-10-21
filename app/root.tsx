b674adb8e6514b5b3096d8688dea91b36fe6d367
Andrew Hyndman <ajhyndman@outlook.com.au>
2023-10-21 00:24:30 -0400

    Configure CSS Bundling

--- app/root.tsx
+++ app/root.tsx
@@ -1,24 +1,17 @@
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-} from "@remix-run/react";
+import { cssBundleHref } from '@remix-run/css-bundle';
+import { Links, LiveReload, Meta, Outlet, Scripts } from '@remix-run/react';
+
+export const links = () => [{ rel: 'stylesheet', href: cssBundleHref! }];
 
 export default function App() {
   return (
     <html>
       <head>
-        <link
-          rel="icon"
-          href="data:image/x-icon;base64,AA"
-        />
+        <link rel="icon" href="data:image/x-icon;base64,AA" />
         <Meta />
         <Links />
       </head>
       <body>
-        <h1>Hello world!</h1>
         <Outlet />
 
         <Scripts />
