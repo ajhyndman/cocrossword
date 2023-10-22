e5c99c5e66e0f3bd2fbdf22bdb47b63c45e846e4
Andrew Hyndman <ajhyndman@outlook.com.au>
2023-10-22 14:38:04 -0400

    kafkajs/client.ts: Add helpful error message

--- app/kafkajs/client.ts
+++ app/kafkajs/client.ts
@@ -3,18 +3,26 @@ import { Kafka, Consumer, Producer, logLevel } from 'kafkajs';
 let singleton: Promise<{ consumer: Consumer; producer: Producer }>;
 
 export function getKafkaClient() {
+  if (
+    !process.env.KAFKA_BROKER_URL ||
+    !process.env.KAFKA_SASL_USERNAME ||
+    !process.env.KAFKA_SASL_PASSWORD
+  ) {
+    throw new Error('Whoops, you need to configure Kafka!  Please check your .env file.');
+  }
+
   if (!singleton) {
     singleton = new Promise(async (resolve) => {
       const kafka = new Kafka({
         clientId: `crossword-app-${process.env.NODE_ENV}`,
-        brokers: [process.env.KAFKA_BROKER_URL],
+        brokers: [process.env.KAFKA_BROKER_URL!],
         connectionTimeout: 45000,
         logLevel: logLevel.ERROR,
         ssl: true,
         sasl: {
           mechanism: 'plain', // scram-sha-256 or scram-sha-512
-          username: process.env.KAFKA_SASL_USERNAME,
-          password: process.env.KAFKA_SASL_PASSWORD,
+          username: process.env.KAFKA_SASL_USERNAME!,
+          password: process.env.KAFKA_SASL_PASSWORD!,
         },
       });
       const producer = kafka.producer();
