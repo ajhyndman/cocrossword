ee6d5f23d1ef5b4269cb9e9c3255d3fb4a80fd55
Andrew Hyndman <ajhyndman@outlook.com.au>
2023-10-22 15:41:21 -0400

    Implement useEventSourceReducer

---
+++ app/util/useEventSourceReducer.ts
@@ -0,0 +1,36 @@
+import { useEffect, useRef, useState } from 'react';
+import debounce from 'lodash.debounce';
+
+export function useEventSourceReducer<S, E>(
+  route: string,
+  reducer: (state: S | undefined, message: E) => S
+) {
+  const eventSource = useRef<EventSource>();
+  const state = useRef<S>();
+
+  const [snapshot, setSnapshot] = useState<S>();
+
+  // Don't trigger a react re-render on _every_ update
+  const setSnapshotDebounced = debounce((nextState: S) => setSnapshot(nextState), 100);
+
+  useEffect(() => {
+    eventSource.current = new EventSource(route);
+  }, []);
+
+  useEffect(() => {
+    const source = eventSource.current;
+    if (!source) return;
+
+    const handleEvent = (event: MessageEvent<E>) => {
+      const nextState = reducer(state.current, event.data);
+      state.current = nextState;
+
+      setSnapshotDebounced(nextState);
+    };
+
+    source.addEventListener('message', handleEvent);
+    return () => source.removeEventListener('message', handleEvent);
+  }, []);
+
+  return snapshot;
+}
