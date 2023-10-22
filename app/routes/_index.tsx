238b20b7a3fbe0e4455af108550f13fb88f0a27f
Andrew Hyndman <ajhyndman@outlook.com.au>
2023-10-22 14:46:26 -0400

    Proof of concept: SSE Kafka route

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,31 @@
+import type { ActionFunctionArgs } from '@remix-run/node';
+import { useEventSource } from 'remix-utils/sse/react';
+import { json } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+
 import DropZone from '~/components/DropZone';
 import styles from './_index.module.css';
 
-export default () => (
-  <div className={styles.container}>
-    <DropZone onChange={(event) => console.log(event.target.files)} />
-    <p className={styles.caption}>Upload a .puz file to start a new game</p>
-  </div>
-);
+export async function action({ params }: ActionFunctionArgs) {
+  return {};
+}
+
+export async function loader() {
+  return json({ data: 1234 });
+}
+
+export default () => {
+  const data = useLoaderData();
+  const message = useEventSource('/sse/kafka', { event: 'message' });
+
+  console.log(data);
+  console.log(message);
+
+  return (
+    <div className={styles.container}>
+      <DropZone onChange={(event) => console.log(event.target.files)} />
+      <p className={styles.caption}>Upload a .puz file to start a new game</p>
+      <p>{message}</p>
+    </div>
+  );
+};
---
+++ app/routes/sse.kafka.ts
@@ -0,0 +1,14 @@
+import { LoaderFunctionArgs } from '@remix-run/node';
+import { messageLog } from '~/kafkajs/index';
+
+import { eventStream } from 'remix-utils/sse/server';
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  return eventStream(request.signal, (send) => {
+    const unsubscribe = messageLog.subscribe((message) => {
+      send({ event: 'message', data: message.value });
+    });
+
+    return unsubscribe;
+  });
+}
