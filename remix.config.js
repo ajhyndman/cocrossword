c0b83dc29574a8fddfa7f12c3edd91deac492a29
Andrew Hyndman <ajhyndman@outlook.com.au>
2023-10-21 22:34:26 -0400

    remix.config.js: Include buffer polyfill

--- remix.config.js
+++ remix.config.js
@@ -1,13 +1,16 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   // appDirectory: "app",
-  assetsBuildDirectory: "public/dist",
-  ignoredRouteFiles: ["**/.*", "**/*.css"],
-  publicPath: "/dist/",
+  assetsBuildDirectory: 'public/dist',
+  ignoredRouteFiles: ['**/.*', '**/*.css'],
+  publicPath: '/dist/',
   // routes(defineRoutes) {
   //   return defineRoutes((route) => {
   //     route("/somewhere/cool/*", "catchall.tsx");
   //   });
   // },
-  serverBuildPath: "dist/index.js",
+  browserNodeBuiltinsPolyfill: {
+    modules: { buffer: true },
+  },
+  serverBuildPath: 'dist/index.js',
 };
