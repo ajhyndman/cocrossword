b2de01898c74e1e02674567546001fa83854241f
Andrew Hyndman <ajhyndman@outlook.com.au>
2023-10-22 22:42:12 -0400

    DELETE app/store-old

--- app/store-old/store.ts
+++
@@ -1,8 +0,0 @@
-import { legacy_createStore } from 'redux';
-
-import { reducer } from './reducer';
-
-export const createStore = () => {
-  const store = legacy_createStore(reducer);
-  return store;
-};
