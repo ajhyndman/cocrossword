907d8e6f271c13d922d7a8df61d72ac7fb0d8955
Andrew Hyndman <ajhyndman@outlook.com.au>
2023-11-05 14:25:47 -0500

    store/puzzle: Add additional comments

--- app/store/puzzle.tsx
+++ app/store/puzzle.tsx
@@ -22,12 +22,17 @@ const reducer = (state: State, { type, payload }: Action) => {
   switch (type) {
     case 'NEW_PUZZLE':
       return { ...state, puzzle: payload };
+
     case 'CELL_CHANGED':
+      // if no puzzle set yet, ignore this action
       if (!state.puzzle?.state) return state;
+      // if action has no contents, ignore this action
       if (payload.value == null) return state;
+      // update the character at the given position
       const nextState = [...state.puzzle.state];
       nextState[payload.index] = payload.value.slice(0, 1).toUpperCase();
       return { ...state, puzzle: { ...state.puzzle, state: nextState.join('') } };
+
     default:
       return state;
   }
