d63cc4ef338ddcde7b8f3363e3243cb511ff6bb1
Andrew Hyndman <ajhyndman@outlook.com.au>
2023-11-05 22:52:36 -0500

    SolverAppBar: Check solution and navigate to chat

--- app/components/SolverAppBar.tsx
+++ app/components/SolverAppBar.tsx
@@ -1,10 +1,23 @@
+import { Link } from '@remix-run/react';
+
 import { useSelectionStore } from '~/store/selection';
 import BottomAppBar from './BottomAppBar';
 import FloatingActionButton from './FloatingActionButton';
 import IconButton from './IconButton';
+import { usePuzzleStore } from '~/store/puzzle';
+import { isCorrect } from '@ajhyndman/puz';
 
 export default () => {
   const { dispatch, selection } = useSelectionStore();
+  const { puzzle } = usePuzzleStore();
+
+  const checkSolution = () => {
+    if (isCorrect(puzzle!)) {
+      window.alert('Congratulations!\n\nYou solved this puzzle.');
+    } else {
+      window.alert('Nope!\n\nThis puzzle is not yet correct. Please try again');
+    }
+  };
 
   const handleRotateSelectionClick = () => {
     // if element is already selected, "focus" event won't be triggered
@@ -15,10 +28,12 @@ export default () => {
     <BottomAppBar
       left={
         <>
-          <IconButton name="chat" />
-          <IconButton name="info" />
-          <IconButton name="check_box" />
-          <IconButton name="edit" />
+          <Link relative="path" to="../chat">
+            <IconButton name="chat" />
+          </Link>
+          <IconButton name="check_box" onClick={checkSolution} />
+          {/* <IconButton name="info" /> */}
+          {/* <IconButton name="edit" /> */}
         </>
       }
       right={
