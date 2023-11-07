3e2843e7c05e5e037473961efe36a1fe21bd580c
Andrew Hyndman <ajhyndman@outlook.com.au>
2023-11-07 00:36:31 -0500

    Revert "Skip previously filled in cells when typing"
    This reverts commit af175f628b6d4da33fa16c721fec9abfe9333248.

--- app/util/cursor.ts
+++ app/util/cursor.ts
@@ -2,28 +2,23 @@ import type { Puzzle } from '@ajhyndman/puz';
 import type { Selection } from '~/store/selection';
 
 export function getNextIndex(puzzle: Puzzle, selection: Selection) {
-  if (selection.index == null || puzzle.state == null) return selection.index;
+  if (selection.index == null) return selection.index;
   let nextIndex = selection.index;
-  do {
-    if (selection.direction === 'row') {
-      nextIndex += 1;
-    } else {
-      nextIndex += puzzle.width;
-    }
-    if (
-      // off the bottom of the puzzle
-      nextIndex > puzzle.solution.length ||
-      // next cell is a black square
-      puzzle.solution[nextIndex] === '.' ||
-      // off the right edge of the puzzle
-      (selection.direction === 'row' && nextIndex % puzzle.width === 0)
-    ) {
-      return selection.index;
-    }
-  } while (
-    // keep advancing if cell is not empty
-    puzzle.state?.[nextIndex] !== '-'
-  );
+  if (selection.direction === 'row') {
+    nextIndex += 1;
+  } else {
+    nextIndex += puzzle.width;
+  }
+  if (
+    // off the bottom of the puzzle
+    nextIndex > puzzle.solution.length ||
+    // next cell is a black square
+    puzzle.solution[nextIndex] === '.' ||
+    // off the right edge of the puzzle
+    (selection.direction === 'row' && nextIndex % puzzle.width === 0)
+  ) {
+    return selection.index;
+  }
   return nextIndex;
 }
 
