8a23013bb5a7ec23cbd47adb84c16a45d2b49555
Andrew Hyndman <ajhyndman@outlook.com.au>
2023-11-09 02:01:53 -0500

    Add batch dispatch endpoint

---
+++ app/routes/dispatch.ts
@@ -0,0 +1,12 @@
+import { ActionFunctionArgs } from '@remix-run/node';
+import { dispatch } from '~/kafkajs';
+
+/**
+ * Accept a list of actions and dispatch them all to kafka
+ */
+export async function action({ request }: ActionFunctionArgs) {
+  const keyActions: [string, any] = await request.json();
+  keyActions.forEach(([key, action]) => dispatch(key, action));
+
+  return null;
+}
