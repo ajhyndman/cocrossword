1300fc460b9393eb804995e774c3beb360464f35
Andrew Hyndman <ajhyndman@outlook.com.au>
2023-11-11 17:34:08 -0500

    chore: FloatingActionButton supports size options

--- app/components/FloatingActionButton.tsx
+++ app/components/FloatingActionButton.tsx
@@ -1,12 +1,21 @@
+import classNames from 'classnames';
+
 import styles from './FloatingActionButton.module.css';
 
 type Props = {
-  onClick?: () => void;
   name: string;
+  onClick?: () => void;
+  size?: 'small' | 'normal' | 'large';
 };
 
-export default ({ name, onClick }: Props) => (
-  <button className={styles.button} onClick={onClick}>
-    <span className={`material-icons ${styles.icon}`}>{name}</span>
+export default ({ name, onClick, size }: Props) => (
+  <button
+    className={classNames(styles.button, {
+      [styles.small]: size === 'small',
+      [styles.large]: size === 'large',
+    })}
+    onClick={onClick}
+  >
+    <span className={classNames('material-icons', styles.icon)}>{name}</span>
   </button>
 );
