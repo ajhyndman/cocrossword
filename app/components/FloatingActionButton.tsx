966d1ff95311b3653534b67961a8107fd5eeb772
Andrew Hyndman <ajhyndman@outlook.com.au>
2023-11-11 18:18:24 -0500

    chore: Add additional options to FAB

--- app/components/FloatingActionButton.tsx
+++ app/components/FloatingActionButton.tsx
@@ -4,16 +4,20 @@ import styles from './FloatingActionButton.module.css';
 
 type Props = {
   name: string;
+  onBlur?: () => void;
   onClick?: () => void;
   size?: 'small' | 'normal' | 'large';
+  transparent?: boolean;
 };
 
-export default ({ name, onClick, size }: Props) => (
+export default ({ name, onBlur, onClick, size, transparent }: Props) => (
   <button
     className={classNames(styles.button, {
       [styles.small]: size === 'small',
       [styles.large]: size === 'large',
+      [styles.transparent]: transparent,
     })}
+    onBlur={onBlur}
     onClick={onClick}
   >
     <span className={classNames('material-icons', styles.icon)}>{name}</span>
