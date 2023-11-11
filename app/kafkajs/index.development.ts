c1fe3c6e4463e218141e45be2adcd3cf792dd854
Andrew Hyndman <ajhyndman@outlook.com.au>
2023-11-11 16:58:57 -0500

    chore: persist development kafka state across rebuilds

--- app/kafkajs/index.development.ts
+++ app/kafkajs/index.development.ts
@@ -1,16 +1,41 @@
-import { Log } from './log';
+import fs from 'fs/promises';
+import path from 'path';
+import url from 'url';
 import type { Message } from 'kafkajs';
+
+import { Log } from './log';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+// persist log messages locally in development
+const TEMP_DIR = path.join(__dirname, '__temp__');
+const LOG_FILE = path.join(TEMP_DIR, 'log');
 
 let log: Log<Message>;
 
 export async function dispatch(key: string, action: any) {
   const log = await getMessageLog();
-  log.push({ key, value: JSON.stringify(action) });
+  const message = { key, value: JSON.stringify(action) };
+
+  log.push(message);
+  await fs.appendFile(LOG_FILE, JSON.stringify(message) + '\n');
 }
 
 export async function getMessageLog() {
   if (!log) {
-    log = new Log();
+    try {
+      await fs.mkdir(TEMP_DIR);
+    } catch {
+      // pass
+    }
+    const buffer = await fs.readFile(LOG_FILE, { flag: 'a+' });
+    const text = buffer.toString();
+    const messages = text
+      .split('\n')
+      .filter((line) => line !== '')
+      .map((json) => JSON.parse(json));
+
+    log = new Log(messages);
   }
 
   return log;
