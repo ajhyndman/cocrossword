fd8ca62aecd4a6482baf0a382213825d482fd7e8
Andrew Hyndman <ajhyndman@outlook.com.au>
2023-11-11 16:57:46 -0500

    New component: NavigationTabs

---
+++ app/components/NavigationTabs.tsx
@@ -0,0 +1,29 @@
+import { useOutletContext } from '@remix-run/react';
+import Tabs from './Tabs';
+import { useStore } from '~/store/remote';
+import { useMemo } from 'react';
+
+type Props = {
+  id: string;
+  userId: string;
+};
+
+export default ({ id, userId }: Props) => {
+  const {
+    state: { readReceipts, messages },
+  } = useStore();
+
+  const hasUnreadMessages = useMemo(() => {
+    return messages.length > (readReceipts[userId] ?? 0);
+  }, [userId, readReceipts[userId], messages.length]);
+
+  return (
+    <Tabs
+      tabs={[
+        { icon: 'window', href: `/${id}/puzzle` },
+        { icon: 'chat', href: `/${id}/chat`, notify: hasUnreadMessages },
+        { icon: 'group', href: `/${id}/players` },
+      ]}
+    />
+  );
+};
