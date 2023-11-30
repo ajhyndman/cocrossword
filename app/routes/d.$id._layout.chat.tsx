60c6f000105d8d59a00ec5592b78a76c768892b4
Andrew Hyndman <ajhyndman@outlook.com.au>
2023-11-29 20:57:39 -0500

    feat: add Ko-fi link to chat sidebar

--- app/routes/d.$id._layout.chat.tsx
+++ app/routes/d.$id._layout.chat.tsx
@@ -21,6 +21,20 @@ export default function View() {
             <AppInfo />
           </div>
         </section>
+        <section className={styles.section}>
+          <Title tag="h3">SUPPORT</Title>
+          <p>If you enjoy this website, please consider leaving a tip. </p>
+          <p>
+            <a href="https://ko-fi.com/C0C2RO28J" target="_blank" rel="noreferrer">
+              <img
+                height="36"
+                style={{ border: 0, height: 36 }}
+                src="https://storage.ko-fi.com/cdn/brandasset/kofi_button_stroke.png"
+                alt="Buy Me a Coffee at ko-fi.com"
+              />
+            </a>
+          </p>
+        </section>
       </div>
     </div>
   );
