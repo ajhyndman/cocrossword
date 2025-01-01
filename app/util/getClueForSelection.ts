65cbc5eb4efc08534142ac43b557f5a12182cb9d
Andrew Hyndman <ajhyndman@outlook.com.au>
2025-01-02 00:09:08 +1100

    getClueForSelectionMemo

--- app/util/getClueForSelection.ts
+++ app/util/getClueForSelection.ts
@@ -1,25 +1,41 @@
 import { gridNumbering, Puzzle } from '@ajhyndman/puz';
+import memoize from 'lodash.memoize';
 
 import type { Selection } from '~/store/local/selection';
+
+const getClueForSelectionMemo = memoize(function (
+  solution: Puzzle['solution'],
+  width: Puzzle['width'],
+  direction: Selection['direction'],
+  selectionIndex: Selection['index'],
+): number {
+  if (selectionIndex == null) return -1;
+
+  const numbering = gridNumbering({ solution, width });
+  let clue;
+
+  let index = selectionIndex;
+
+  // find the clue associated with this selection
+  do {
+    clue = numbering[index];
+    index = index - (direction === 'row' ? 1 : width);
+  } while (
+    index >= 0 &&
+    solution[index] !== '.' &&
+    (direction !== 'row' || index % width !== width - 1)
+  );
+  return clue!;
+});
 
 export const getClueForSelection = (
   puzzle: Pick<Puzzle, 'solution' | 'width'>,
   selection: Selection,
 ): number => {
-  if (selection.index == null) return -1;
-
-  const numbering = gridNumbering(puzzle);
-  let clue;
-  let index = selection.index;
-
-  // find the clue associated with this selection
-  do {
-    clue = numbering[index];
-    index = index - (selection.direction === 'row' ? 1 : puzzle.width);
-  } while (
-    index >= 0 &&
-    puzzle.solution[index] !== '.' &&
-    (selection.direction !== 'row' || index % puzzle.width !== puzzle.width - 1)
+  return getClueForSelectionMemo(
+    puzzle.solution,
+    puzzle.width,
+    selection.direction,
+    selection.index,
   );
-  return clue!;
 };
